2fec5cc641c63b35d7fb57b1d43659cbf6d3e9cf
TkachevNikita <ntkachev03@mail.ru>
2024-03-09 17:41:55 +0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonComponent} from "./components/UI/button/button.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ButtonComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/UI/button/button.component.ts
@@ -0,0 +1,32 @@
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+@Component({
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
+    selector: 'IButton',
+    standalone: true,
+    imports: [
+        CommonModule
+    ]
+})
+export class ButtonComponent implements AfterViewInit {
+    @Input()
+    public variant!: string;
+    @ViewChild('buttonElement')
+    public buttonElement!: ElementRef;
+
+    public ngAfterViewInit() {
+        switch (this.variant) {
+            case 'basic':
+                this.buttonElement.nativeElement.classList.add('button__basic');
+                return;
+            case 'primary':
+                this.buttonElement.nativeElement.classList.add('button__primary');
+                return;
+            default:
+                this.buttonElement.nativeElement.classList.add('button__basic');
+                return;
+        }
+    }
+}
