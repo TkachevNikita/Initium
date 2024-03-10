b17beada6bd8392f9280cdc51a756ae9ba23c8e1
TkachevNikita <ntkachev03@mail.ru>
2024-03-10 23:58:50 +0500

    feat: настроил базовый роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClientPageComponent} from "./pages/client/client-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'clients',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        children: [
+            {
+                path: 'clients',
+                component: ClientPageComponent
+            }
+        ]
+    },
+    {
+        path: '*',
+        redirectTo: ''
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonComponent} from "./components/UI/button/button.component";
+import {ClientPageComponent} from "./pages/client/client-page.component";
 
 @NgModule({
     declarations: [
         AppComponent,
+        ClientPageComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ButtonComponent
+        ButtonComponent,
     ],
     providers: [],
     bootstrap: [AppComponent]
