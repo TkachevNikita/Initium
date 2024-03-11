defa2000db0ae213e82d77c949b10cb82c30e00c
TkachevNikita <ntkachev03@mail.ru>
2024-03-11 14:37:12 +0500

    chore: доработки app компонента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {ModalService} from "./services/modal.service";
+import {ButtonComponent} from "./components/UI/button/button.component";
+import {NewClientFormComponent} from "./components/forms/new-client-form.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonComponent} from "./components/UI/button/button.component";
 import {ClientPageComponent} from "./pages/client/client-page.component";
+import {TableComponent} from "./components/UI/table/table.component";
+import {ClientsService} from "./services/clients.service";
+import {HttpClientModule} from "@angular/common/http";
+import {ModalComponent} from "./components/UI/modal/modal.component";
+import {PortalModule} from "@angular/cdk/portal";
+import {NewClientFormComponent} from "./components/forms/new-client-form.component";
+import {InputComponent} from "./components/UI/input/input.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
         AppComponent,
-        ClientPageComponent
+        ClientPageComponent,
+        ModalComponent,
+        NewClientFormComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         ButtonComponent,
+        TableComponent,
+        HttpClientModule,
+        PortalModule,
+        InputComponent,
+        ReactiveFormsModule
     ],
-    providers: [],
+    providers: [
+        ClientsService,
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
