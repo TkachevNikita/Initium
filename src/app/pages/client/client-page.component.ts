da52906e540ba3e9648a5f5944bc610e33823288
TkachevNikita <ntkachev03@mail.ru>
2024-03-11 14:28:50 +0500

    chore: доработки страницы клиентов

--- src/app/pages/client/client-page.component.ts
+++ src/app/pages/client/client-page.component.ts
@@ -1,10 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ClientsService} from "../../services/clients.service";
+import {BehaviorSubject} from "rxjs";
+import {ClientModel} from "../../models/client.model";
 
 @Component({
     selector: 'app-contacts',
     templateUrl: './client-page.component.html',
     styleUrls: ['./client-page.component.scss'],
 })
-export class ClientPageComponent {
+export class ClientPageComponent implements OnInit {
+    public clients$: BehaviorSubject<ClientModel[]> = new BehaviorSubject<ClientModel[]>([]);
+    constructor(private _clientsService: ClientsService) { }
 
+    public ngOnInit() {
+        this._clientsService.getClients()
+            .subscribe(
+                (clients: ClientModel[])  => this.clients$.next(clients)
+            )
+    }
 }
